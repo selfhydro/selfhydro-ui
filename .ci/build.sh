d5e4aeb70b1ef849457e8367555d0cb22cc533fe
Baz <boruch11@gmail.com>
2019-07-30 17:18:59 +1000

    Tag build with param and move release to output folder
    Signed-off-by: Baz <boruch11@gmail.com>

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,9 +2,13 @@
 
 set -ex
 
+VERSION=$1
+
 cd ./selfhydro-ui
 
 npm i
 npm run build
 
+echo $VERSION
 tar -zcvf selfhydro-ui-release-$(echo $VERSION).tar.gz build
+mv selfhydro-ui-release-* ../selfhydro-ui-build
