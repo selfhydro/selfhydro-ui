457248c9e4fb57c77f7e4129531c540afc838250
Baz <boruch11@gmail.com>
2019-07-31 13:21:00 +1000

    Dont tar build artifact
    Signed-off-by: Baz <boruch11@gmail.com>

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,13 +2,9 @@
 
 set -ex
 
-VERSION=$1
-
 cd ./selfhydro-ui
 
 npm i
 npm run build
 
-echo $VERSION
-tar -zcvf selfhydro-ui-release-$(echo $VERSION).tar.gz build
-mv selfhydro-ui-release-* ../selfhydro-ui-build
+mv build ../selfhydro-ui-build/selfhydro-ui-release-$(cat /version/version)
--- .ci/deploy-infra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-cd ./selfhydro-ui
-
-terraform init -input=false
-terraform apply -var-file="infra/prod.tfvars" -input=false -auto-approve
