24ebb135aaffa9a38391bcc8bcbfdf32c83eeea4
Baz <boruch11@gmail.com>
2019-08-12 19:37:17 +1000

    Add global ip address and load balancer to backend bucket
    Signed-off-by: Baz <boruch11@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,23 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+output "gcp_credentials" {
+  value       = google.credentials
+  description = "The credentials for authenticating with gcp."
+  sensitive   = true
+}
+
+resource "google_storage_bucket" "selfhydro-build-artifacts" {
+    name = "selfhydro-build-artifacts"
+    location = "${var.bucket_location}"
+    project = "${var.project_id}"
+    storage_class = "${var.bucket_storage_class}"
+
+    versioning {
+        enabled = "${var.bucket_versioning}"
+    }
+}
+
 resource "google_storage_bucket" "selfhydro-ui-bucket" {
     name = "${var.bucket_name}"
     location = "${var.bucket_location}"
@@ -33,19 +50,23 @@ resource "google_compute_backend_bucket" "selfhydro-ui-backend" {
   enable_cdn  = true
 }
 
-resource "google_storage_bucket" "selfhydro-build-artifacts" {
-    name = "selfhydro-build-artifacts"
-    location = "${var.bucket_location}"
-    project = "${var.project_id}"
-    storage_class = "${var.bucket_storage_class}"
-
-    versioning {
-        enabled = "${var.bucket_versioning}"
-    }
+resource "google_compute_global_address" "selfhydro-ui" {
+  name = "selfhydro-ui"
 }
 
-output "gcp_credentials" {
-  value       = google.credentials
-  description = "The credentials for authenticating with gcp."
-  sensitive   = true
+resource "google_compute_global_forwarding_rule" "selfhydro-ui" {
+  name       = "selfhydro-ui-port-80"
+  ip_address = "${google_compute_global_address.selfhydro-ui.address}"
+  port_range = "80"
+  target     = "${google_compute_target_http_proxy.selfhydro-ui.self_link}"
+}
+
+resource "google_compute_target_http_proxy" "selfhydro-ui" {
+  name    = "selfhydro-ui"
+  url_map = "${google_compute_url_map.selfhydro-ui.self_link}"
+}
+
+resource "google_compute_url_map" "selfhydro-ui" {
+  name        = "selfhydro-ui"
+  default_service = "${google_compute_backend_bucket.selfhydro-ui-backend.self_link}"
 }
