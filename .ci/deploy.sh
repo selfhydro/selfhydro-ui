163a0ddf28403480b99052077d49d2e6307c01f0
Baz <boruch11@gmail.com>
2019-08-13 21:14:14 +1000

    deploy build to firebase
    Signed-off-by: Baz <boruch11@gmail.com>

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-set +e +x
-
-echo ${CREDENTIALS} >> key.json
-unset CREDENTIALS
-
 set -ex
 
-gcloud auth activate-service-account selfhydro-197504@appspot.gserviceaccount.com --key-file key.json --project selfhydro-197504
-gcloud config list
-
 tar -xvzf gcs-artifact-bucket/*.tar.gz
-
-gsutil cp -r build/** gs://www.selfhydro.com
-
-rm key.json
+mv build selfhydro-ui/
+cd ./selfhydro-ui
+firebase deploy --token "$FIREBASE_TOKEN"
