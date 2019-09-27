366069abc2bbae8b1fdb0671651e850feb358bec
Baz <boruch11@gmail.com>
2019-09-27 13:17:25 +1000

    add test for loading temp

--- src/SelfhydroState.test.js
+++ src/SelfhydroState.test.js
@@ -8,11 +8,11 @@ import GetAmbientTemperature from "./Service.js";
 
 jest.mock("./Service.js");
 
-it("should display temperture", async () => {
+it("should display temperture", async done => {
   GetAmbientTemperature.mockImplementation(() => Promise.resolve(12));
 
   const {getByDisplayValue} = render(<SelfhydroState />);
-
+  setImmediate(done);
   await wait(() => getByDisplayValue("12"));
   const temperatureValie = expect(getByDisplayValue("12")).toBe(12);
 });
