fb9b657ae3ec75a5ddba177c7caaed3f86d00cb3
Baz <boruch11@gmail.com>
2019-10-04 15:40:27 +1000

    add auth with firebase config

--- src/Authentication.js
+++ src/Authentication.js
@@ -3,12 +3,23 @@ import React from "react";
 import withFirebaseAuth from "react-with-firebase-auth";
 import * as firebase from "firebase/app";
 import "firebase/auth";
+import firebaseConfig from "./config.js";
 
-function Authentication() {
-  return <div />;
+function Authentication(props) {
+  const {user, signOut, signInWithGoogle} = props;
+  return (
+    <div>
+      {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+      {user ? (
+        <button onClick={signOut}>Sign out</button>
+      ) : (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      )}
+    </div>
+  );
 }
 
-const firebaseApp = firebase.initializeApp();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 console.log("debugging " + firebaseApp);
 const firebaseAppAuth = firebaseApp.auth();
 const providers = {
---
+++ src/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBGl5tDfENYRPvdNwUFJLzw1qk-2hTmcUo",
+  authDomain: "selfhydro-197504.firebaseapp.com",
+  databaseURL: "https://selfhydro-197504.firebaseio.com",
+  projectId: "selfhydro-197504",
+  storageBucket: "selfhydro-197504.appspot.com",
+  messagingSenderId: "268402011423",
+  appId: "1:268402011423:web:8ee40845766d8703e3d42a"
+};
+
+export default firebaseConfig;
